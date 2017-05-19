a4584e427469356b11316679acc9585b29edd640
AshwiniChilukuri <ashwini.chilukuri@sjsu.edu>
2017-05-18 19:49:07 -0700

    Initial upload

---
+++ learning.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+int main()
+{
+std::cout<<"Hi!"<<std::endl<<"I am Excited as its my first cpp program";
+return 0;
+}
+
+#include <iostream>
+//program to accept variable from console
+int main()
+{
+int x;	//declaration
+std::cin.clear();
+std::cout<<"Enter a number" <<std::endl;
+std::cin>>x;
+std::cout<<"you have entered "<<x;
+return 0;
+}
+
